a0053970531d36a08b4a42a2e7f44e9f0ba5d538
Mahmuda04903 <akter15-5024@diu.edu.bd>
2025-07-18 21:06:11 +0600

    Initial commit

---
+++ Calculator/script.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("display");
+
+function press(value) {
+  display.value += value;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
